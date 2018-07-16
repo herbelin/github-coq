6166f95b3eece774c2c7a9d7351d4260c04e76b4
Jason Gross <jgross@mit.edu>
2018-07-16 09:52:47 -0400

    Add overlay for HoTT

---
+++ dev/ci/user-overlays/jasongross-numeral-notation-4.sh
@@ -0,0 +1,4 @@
+if [ "$CI_PULL_REQUEST" = "8064" ] || [ "$CI_BRANCH" = "numeral-notation-4" ]; then
+    HoTT_CI_BRANCH=fix-for-numeral-notations
+    HoTT_CI_GITURL=https://github.com/JasonGross/HoTT.git
+fi
