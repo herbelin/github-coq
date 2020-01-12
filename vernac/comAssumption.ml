aa5078e28def382a6f2074dc9df4c5f15b4b71b2
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2020-01-12 23:32:12 +0100

    Fixes #9412 (Context ignores implicit binders).

--- vernac/comAssumption.ml
+++ vernac/comAssumption.ml
@@ -187,16 +187,24 @@ let do_assumptions ~program_mode ~poly ~scope ~kind nl l =
 let context_subst subst (name,b,t,impl) =
   name, Option.map (Vars.substl subst) b, Vars.substl subst t, impl
 
-let context_insection sigma ~poly ctx =
+let extract_manual_implicit e =
+  CAst.make (Option.map (fun (e,_,(max,_)) ->
+    let open Constrexpr in
+    let na = match e with ExplByPos _ -> Anonymous | ExplByName id -> Name id in
+    (na,max)) e)
+
+let context_insection sigma ~poly int_env ctx =
   let uctx = Evd.universe_context_set sigma in
   let () = DeclareUctx.declare_universe_context ~poly uctx in
   let fn subst (name,_,_,_ as d) =
-    let d = context_subst subst d in
-    let () = match d with
-      | name, None, t, impl ->
+    let (id,b,t,impl) = context_subst subst d in
+    let impargs = implicits_of_decl_in_internalization_env id int_env in
+    let impargs = List.map extract_manual_implicit impargs in
+    let () = match b with
+      | None ->
         let kind = Decls.Context in
-        declare_variable false ~kind t [] impl (CAst.make name)
-      | name, Some b, t, impl ->
+        declare_variable false ~kind t impargs impl (CAst.make id)
+      | Some b ->
         (* We need to get poly right for check_same_poly *)
         let univs = if poly then Polymorphic_entry ([| |], Univ.UContext.empty)
           else Monomorphic_entry Univ.ContextSet.empty
@@ -207,7 +215,7 @@ let context_insection sigma ~poly ctx =
         let kind = Decls.(IsDefinition Definition) in
         let _ : GlobRef.t =
           Declare.declare_entry ~name ~scope:Locality.Discharge
-            ~kind ~impargs:[] ~uctx entry
+            ~kind ~impargs ~uctx entry
         in
         ()
     in
@@ -216,7 +224,7 @@ let context_insection sigma ~poly ctx =
   let _ : Vars.substl = List.fold_left fn [] ctx in
   ()
 
-let context_nosection sigma ~poly ctx =
+let context_nosection sigma ~poly int_env ctx =
   let univs =
     match ctx, poly with
     | [_], _ | _, true -> Evd.univ_entry ~poly sigma
@@ -228,8 +236,10 @@ let context_nosection sigma ~poly ctx =
       Monomorphic_entry Univ.ContextSet.empty
   in
   let fn subst d =
-    let (name,b,t,_impl) = context_subst subst d in
+    let (name,b,t,impl) = context_subst subst d in
     let kind = Decls.(IsAssumption Logical) in
+    let impls = implicits_of_decl_in_internalization_env name int_env in
+    let impls = List.map extract_manual_implicit impls in
     let decl = match b with
       | None ->
         Declare.ParameterEntry (None,(t,univs),None)
@@ -241,6 +251,7 @@ let context_nosection sigma ~poly ctx =
       else Locality.ImportNeedQualified
     in
     let cst = Declare.declare_constant ~name ~kind ~local decl in
+    let () = maybe_declare_manual_implicits false (GlobRef.ConstRef cst) impls in
     let () = Declare.assumption_message name in
     let env = Global.env () in
     (* why local when is_modtype? *)
@@ -256,7 +267,7 @@ let context_nosection sigma ~poly ctx =
 let context ~poly l =
   let env = Global.env() in
   let sigma = Evd.from_env env in
-  let sigma, (_, ((_env, ctx), impls)) = interp_context_evars ~program_mode:false env sigma l in
+  let sigma, (int_env, ((_env, ctx), impls)) = interp_context_evars ~program_mode:false env sigma l in
   (* Note, we must use the normalized evar from now on! *)
   let ce t = Pretyping.check_evars env sigma t in
   let () = List.iter (fun decl -> Context.Rel.Declaration.iter_constr ce decl) ctx in
@@ -281,5 +292,5 @@ let context ~poly l =
       ctx
   in
   if Global.sections_are_opened ()
-  then context_insection sigma ~poly ctx
-  else context_nosection sigma ~poly ctx
+  then context_insection sigma ~poly int_env ctx
+  else context_nosection sigma ~poly int_env ctx
