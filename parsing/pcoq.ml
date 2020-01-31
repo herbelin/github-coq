02a88f7934bd9724b404c8156e8ea5091e9c5c63
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2020-01-31 01:04:07 +0100

    Adding bignat to parse positive numbers; bigint now includes negative ints.

--- parsing/pcoq.ml
+++ parsing/pcoq.ml
@@ -393,6 +393,7 @@ module Prim =
     let ident = gec_gen "ident"
     let natural = gec_gen "natural"
     let integer = gec_gen "integer"
+    let bignat = Entry.create "Prim.bignat"
     let bigint = Entry.create "Prim.bigint"
     let string = gec_gen "string"
     let lstring = Entry.create "Prim.lstring"
