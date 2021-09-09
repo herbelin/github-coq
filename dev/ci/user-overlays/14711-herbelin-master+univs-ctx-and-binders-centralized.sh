c8a48c7f7c416f8eede967745c0b60ffde34afb3
Hugo Herbelin <Hugo.Herbelin@inria.fr>
2021-09-10 00:03:20 +0200

    Adding overlays

---
+++ dev/ci/user-overlays/14711-herbelin-master+univs-ctx-and-binders-centralized.sh
@@ -0,0 +1,4 @@
+overlay equations https://github.com/herbelin/Coq-Equations master+adapt-coq-pr14711-univs-ctx-and-binders-centralized 14711
+overlay elpi https://github.com/herbelin/coq-elpi coq-master+adapt-coq-pr14711-univs-ctx-and-binders-centralized 14711
+overlay paramcoq https://github.com/herbelin/paramcoq master+adapt-coq-pr14711-univs-ctx-and-binders-centralized 14711
+overlay Mtac2 https://github.com/herbelin/Mtac2 master+adapt-coq-pr14711-univs-ctx-and-binders-centralized 14711
