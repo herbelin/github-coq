15d534b18ac4ca98282b87b7963a9c3424dc2e93
Valerii Huhnin <olympichek1@gmail.com>
2026-07-10 16:59:36 +0000

    Add downstream overlays for global-fix retyping

---
+++ dev/ci/user-overlays/ppedrot-global-fix-retyping.sh
@@ -0,0 +1,5 @@
+# Validation overlays for the global-fix retyping branch.
+# mathcomp: 2f70ce6f Adapt divisor membership rewrites to folded fixpoints
+# fourcolor: b17cfa1 Adapt membership rewrite to folded fixpoints
+overlay mathcomp https://github.com/olympichek/math-comp ppedrot-global-fix-retyping-master 99999 ppedrot-global-fix-retyping-master
+overlay fourcolor https://github.com/olympichek/fourcolor ppedrot-global-fix-retyping-master 99999 ppedrot-global-fix-retyping-master
